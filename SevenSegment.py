cf1a3b88e493cb6358046ee90b62a5f5b71c93ed
Eric McConville <erichasemail@gmail.com>
2012-12-10 11:25:14 -0600

    Moved methods around, and added doctest

--- SevenSegment.py
+++ SevenSegment.py
@@ -1,6 +1,16 @@
 import _HT16K33
 
 class SevenSegment(_HT16K33.Base):
+  '''
+     Four Seven-segment digit LED display for Adafruit's HT16K33 I2C backpack
+     - - -
+     [Blue](http://adafruit.com/products/881)
+     [Green](http://adafruit.com/products/880)
+     [Red](http://adafruit.com/products/878)
+     [Yellow](http://adafruit.com/products/879)
+     [White](http://adafruit.com/products/1002)
+  '''
+  
   TOP_BAR = 0x01
   MIDDLE_BAR = 0x40
   BOTTOM_BAR = 0x08
@@ -26,23 +36,140 @@ class SevenSegment(_HT16K33.Base):
     0x39 : 0x67  # 9
   }
   
+  def alterSingleLED(self,position=0,new_byte=0x00,action=None):
+    '''
+       Manipulate single LED in character position
+       - position (0..3)
+       - new_byte (0..0xFF)
+       - action   ("or","xor","andnot")
+       
+       Example:
+       >>> digit = SevenSegment().setUp()    # Set-up
+       >>> digit.setDigit(0,0x5B)            # Assign "2" character to first digit
+       >>> digit.alterSingleLED(0,0x80,"or") # Alter first digit to include period
+    '''
+    position=int(position) % 4
+    digit_address = self.getDigitAddressByIndex(position)
+    byte = self.bus.read_byte_data(self.address,digit_address)
+    if action == "or":
+        byte |= new_byte
+    elif action == "xor":
+        byte ^= new_byte
+    elif action == "andnot":
+        byte &= ~new_byte
+    else:
+        byte = new_byte
+    self.bus.write_byte_data(self.address, digit_address, byte)
+  
   def getDigitAddressByIndex(self,index=0):
+    '''
+      Retrive address by position
+      - index 
+      
+      Example:
+      >>> digit = SevenSegment()
+      >>> digit.getDigitAddressByIndex(1)
+      2
+    '''
     return self.DIGIT_ADDRESS[int(index) % len(self.DIGIT_ADDRESS)]
   
-  def setDigit(self,position=0,value=0x00):
-    self.bus.write_byte_data(self.address,self.getDigitAddressByIndex(position),int(value) % 0x100)
-    
   def chrToInt(self,character):
+    '''
+       Convert character to LED display integer
+       
+       Each available character will have the LED display
+       integer assigned to the CHARACTER_MAP key matching
+       character's order. Any character not present in 
+       CHARACTER_MAP will return 0x00 (clear LED integer.)
+       
+       - character (see CHARACTER_MAP)
+       
+       Example:
+       >>> digit = SevenSegment().setUp()
+       >>> digit.chrToInt(8)
+       127
+       >>> digit.chrToInt("T")
+       0
+    '''
     integer=0x00
     try:
       integer=self.CHARACTER_MAP[ord(str(character))]
     except KeyError:
       pass
     return integer
+  
   def readAtIndex(self,index=0):
-    self.bus.read_byte_data(self.address,self.getDigitAddressByIndex(index))
+    '''
+       Return LED value currently in devices EEPROM
+       - index
+       
+       Example:
+       >>> digit = SevenSegment().setUp()
+       >>> digit.setDigit(0,0xFF)
+       >>> digit.readAtIndex(0)
+       255
+    '''
+    return self.bus.read_byte_data(self.address,self.getDigitAddressByIndex(index))
+  
+  def setDigit(self,position=0,value=0x00):
+    '''
+       Assign LED display to digit
+       - position
+       - value
+       
+       Example:
+       >>> digit = SevenSegment().setUp()
+       >>> digit.setDigit(0,0x06) # 1
+       >>> digit.setDigit(1,0x5B) # 2
+       >>> digit.setDigit(2,0x4F) # 3
+       >>> digit.setDigit(3,0x66) # 4
+    '''
+    self.bus.write_byte_data(self.address,self.getDigitAddressByIndex(position),int(value) % 0x100)
+  
   def turnOnColon(self):
+    '''
+       Enable colon symbol
+       
+       Example:
+       >>> digit = SevenSegment().setUp()
+       >>> digit.turnOnColon()
+    '''
     self.bus.write_byte_data(self.address,self.COLON_ADDRESS,0xFF)
+  
   def turnOffColon(self):
+    '''
+       Disable colon symbol
+       
+       Example:
+       >>> digit = SevenSegment().setUp()
+       >>> digit.turnOffColon()
+    '''
     self.bus.write_byte_data(self.address,self.COLON_ADDRESS,0x00)
   
+  def turnOnPeriodAtPosition(self,position=0):
+    '''
+       Enable period symbol at given position
+    '''
+    self.alterSingleLED(position,self.PERIOD,"or")
+  
+  def turnOffPeriodAtPosition(self,position=0):
+    '''
+       Disable period symbol at given position
+    '''
+    self.alterSingleLED(position,self.PERIOD,"andnot")
+  
+  def writeDigit(self,position,char=None):
+    '''
+       Write single character to a given postion
+       
+       Example:
+       >>> digit = SevenSegment().setUp()
+       >>> for i in range(4):
+       ...   digit.writeDigit(i,i)
+    '''
+    self.setDigit(position,self.chrToInt(char))
+  
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
